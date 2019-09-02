4ea787be4b5bd8d7779b954d52aa52f5682e18b4
Avishai Sade <sade.edit@gmail.com>
2019-09-02 18:07:57 +0300

    pages

--- functions.php
+++ functions.php
@@ -11,6 +11,9 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features (){
   add_theme_support('title-tag');
+  register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
+  register_nav_menu( 'footerLocation1', 'Footer Location1' );
+  register_nav_menu( 'footerLocation2', 'Footer Location2' );
 }
 
 add_action('after_setup_theme', 'university_features');
