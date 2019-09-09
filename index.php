e19331619f9fb68ba40a987e5f481edd2964bf0b
Avishai Sade <sade.edit@gmail.com>
2019-09-09 19:20:16 +0300

    adding events

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-    get_header(); 
-?>
+
+get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('./images/ocean.jpg') ?>;"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Welcome to our blog! </h1>
-      <div class="page-banner__intro">
-        <p>Keep up with our latest news.</p>
-      </div>
-    </div>  
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Welcome to our blog!</h1>
+    <div class="page-banner__intro">
+      <p>Keep up with our latest news.</p>
+    </div>
+  </div>  
 </div>
 
 <div class="container container--narrow page-section">
@@ -30,11 +30,10 @@
 
     </div>
   <?php }
-    echo paginate_links();
+  echo paginate_links();
 ?>
 </div>
 
-  
-<?php
-    get_footer();
-  ?>
+<?php get_footer();
+
+?>
